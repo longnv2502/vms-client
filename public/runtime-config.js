0eb669e8e763f872abdbc6369b7524abcf4222dc
Nguyen Van Long (K15 HL) <longnvhe151244@fpt.edu.vn>
2024-01-27 22:19:30 +0700

    update evn dev

--- public/runtime-config.js
+++ public/runtime-config.js
@@ -1,1 +1,1 @@
-window.__RUNTIME_CONFIG__ = {"VITE_API_SERVER_URL":"https://api-vms.azurewebsites.net/api/v1","VITE_STORAGE_URL":"https://vmscloudstorage.blob.core.windows.net/vms-file-storage/","VITE_BASE_PATH":""};
+window.__RUNTIME_CONFIG__ = {"VITE_API_SERVER_URL":"http://localhost:8000/api/v1","VITE_STORAGE_URL":"https://vmscloudstorage.blob.core.windows.net/vms-file-storage/","VITE_BASE_PATH":""};
